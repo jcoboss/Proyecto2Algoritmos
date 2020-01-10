50c087001caf10d20129cc25ec79a76061b78f2f
enmanuelmag <emmanu2006@hotmail.com>
2020-01-10 11:41:38 -0500

    Implemetacion camara ArcballCamera

--- Vispy.py
+++ Vispy.py
@@ -3,11 +3,13 @@ import numpy as np
 from vispy import scene
 from vispy.scene import visuals
 import vispy
+from vispy import color
+from vispy.color import ColorArray
 import Ambiente as ab
 
 # create data cloud
 campo = ab.obtenerCampo()
-ab.insertarObstaculosCumulos(campo,"SOBRESATURADO")
+ab.insertarObstaculosCubos(campo,"SOBRESATURADO")
 
 
 pos = np.zeros((100, 3), dtype=int)
@@ -33,9 +35,14 @@ The camera auto-rotates to make the bottom point down, manual rolling can be per
 """
 view = canvas.central_widget.add_view()
 flyCam = scene.cameras.FlyCamera(fov=90)
-flyCam.scale_factor = 1000000
-view.camera = flyCam
+flyCam.scale_factor = 100000
+flyCam.center = (99,99,99)
 
+
+cam1 = scene.cameras.MagnifyCamera()
+cam2= scene.cameras.ArcballCamera(fov=90.0, distance=250)
+
+view.camera = cam2
 scatter = visuals.Markers(parent=view.scene)
 
 scatter.antialias = 1
@@ -44,6 +51,11 @@ scatter.set_gl_state(depth_test=True, blend=True, blend_func=('src_alpha', 'one_
 
 # Add axes
 axis = visuals.XYZAxis(parent=view.scene)
+#print(vispy.color.get_colormap)
+
+r = ColorArray('red')
+#map = vispy.color.Colormap(r)
+#newAxis = visuals.ColorBar(parent=view.scene, size=10000, cmap=r, orientation='bottom')
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
     canvas.app.run()
